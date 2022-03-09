cb9824b5fbdb62b7c92b52f954689e15d2dab7fe
Diogo Ferreira Oliveira <99719843+diogofoliveir@users.noreply.github.com>
2022-03-09 10:44:15 +0100

    fixscroll2

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-//      function scrollDown(target) {
-//      console.log(target)
-//      window.scrollBy(0,10);
-//      scrolldelay = setTimeout(scrollDown,1);
-// }
 
-// $(document).ready(function (){
-//     $("#click").click(function (){
-//         $('html, body').animate({
-//             scrollTop: $("#about").offset().top
-//         }, 2000);
-//     });
-// });
+$(document).ready(function (){
+   $("#click").click(function (){
+      $('html, body').animate({
+         scrollTop: $("#about").offset().top
+      }, 2000);
+   });
+});
+
+
+
+$(document).ready(function (){
+$("#click1").click(function (){
+   $('html, body').animate({
+      scrollTop: $("#work").offset().top
+   }, 2000);
+});
+});
+
+
+
+$(document).ready(function (){
+$("#click2").click(function (){
+   $('html, body').animate({
+      scrollTop: $("#shop").offset().top
+   }, 2000);
+});
+});
+
+
+
+$(document).ready(function (){
+$("#click3").click(function (){
+   $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+   }, 2000);
+});
+});
