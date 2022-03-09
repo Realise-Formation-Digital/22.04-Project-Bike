67bf7a289ce4c46c39f0b9d3c90b9749e43b4d73
Sam <Sami_kherbache@hotmail.com>
2022-03-09 10:03:29 +0100

    fix scroll 1

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function scrollDown(target) {
-  console.log(target)
-}
+//      function scrollDown(target) {
+//      console.log(target)
+//      window.scrollBy(0,10);
+//      scrolldelay = setTimeout(scrollDown,1);
+// }
+
+// $(document).ready(function (){
+//     $("#click").click(function (){
+//         $('html, body').animate({
+//             scrollTop: $("#about").offset().top
+//         }, 2000);
+//     });
+// });
