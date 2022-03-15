5a622ebd86d54cc2f75a38b15343e359abc3caa4
Y0NAT <ezechiel.yonathan.favre@gmail.com>
2022-03-15 14:16:14 +0100

    WAZZAAAAAA

--- www/index.php
+++ www/index.php
@@ -240,21 +240,38 @@
             </h2>
 
             <!-- formulaire de contact -->
-            <form method="post">
-               <p>
-               <strong>Nom<span style="color: #ff0000;">*</span> :</strong> <label for="nom"> </label> <input id="nom" name="nom" size="28" type="text" /> 
-               <strong>Prénom :</strong> <label for="prenom"></label> <input id="prenom" name="prenom" size="27" type="text" /> <br /><br /> 
-               <strong>Mail <span style="color: #ff0000;">*</span> : </strong><br /> <label for="mail"> </label><input id="mail" name="mail" size="81" type="text" /></p>
-               <p>Pour quelle raison nous contactez-vous ?</p>
-               <label for="motif"></label> <select id="motif" name="motif"> <option value="reglement">Pour un renseignement</option> 
-               <option value="enquete_civile">Pour un article</option> 
-               <option value="recouvrement">Pour autre chose</option> 
-                </select>
-               <p>Message <span style="color: #ff0000;">*</span> :</p>
-               <p><label for="message"></label> <textarea id="message" cols="52" rows="7" name="message"></textarea></p>
-               <input type="reset" value="Effacer" /> <input type="submit" value="Envoyer" />
-               <p> </p>
-            </form>
+               <!-- Wrapper container -->
+                  <div class="container py-4">
+
+                     <!-- Bootstrap 5 starter form -->
+                     <form id="contactForm">
+                     
+                     <!-- Name input -->
+                     <div class="mb-3">
+                        <label class="form-label" for="name">Name</label>
+                        <input class="form-control" id="name" type="text" placeholder="Name" />
+                     </div>
+                     
+                     <!-- Email address input -->
+                     <div class="mb-3">
+                        <label class="form-label" for="emailAddress">Email Address</label>
+                        <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" />
+                     </div>
+                     
+                     <!-- Message input -->
+                     <div class="mb-3">
+                        <label class="form-label" for="message">Message</label>
+                        <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
+                     </div>
+                     
+                     <!-- Form submit button -->
+                     <div class="d-grid">
+                        <button class="btn btn-primary btn-lg" type="submit">Submit</button>
+                     </div>
+                     
+                     </form>
+                     
+                     </div> 
             <!-- fin du formulaire de contact -->
 
       </section>
