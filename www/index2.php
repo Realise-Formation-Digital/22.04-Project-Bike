fcd744b4ef5436f8163743f04fa25eaea74cf3da
Vinko Roditi <vinko.d.roditi@gmail.com>
2022-03-15 15:55:22 +0100

    fix: info from csv

--- www/index2.php
+++ www/index2.php
@@ -1,25 +1,52 @@
 <?php
+$array=[];
 function pre_r($array){
     echo'<pre>';
     print_r($array);
     echo'</pre>';
+    return $array;
 }
 
 
 
 
 
-//prendre le fichier csv
-$filename = "users.csv";
-//écrire dans le fichier csv
-$f = fopen($filename, 'w');
-if ($f === false) {
-	die('Error opening the file ' . $filename);
+
+if (isset($_POST["exampleInputEmail1"])) {
+    $tableau = [$_POST["exampleInputEmail1"],$_POST["exampleInputPassword1"] ];
+    //prendre le fichier csv
+    $filename = "./file.csv";
+    //écrire dans le fichier csv
+    $f = fopen($filename, 'w');
+    if ($f === false) {
+        die('Error opening the file ' . $filename);
+    }
+    fputcsv($f, $tableau);
+    fclose($f);
+
 }
-fputs($f, (chr(0xEF) . chr(0xBB) . chr(0xBF))); // support unicode
+// if(isset($_Post["exampleInputPassword1"])){
+//     $tableau = [$_POST["exampleInputPassword1"], ];
+//      //prendre le fichier csv
+//      $filename = "./users.csv";
+//      //écrire dans le fichier csv
+//      $f = fopen($filename, 'w');
+//      if ($f === false) {
+//          die('Error opening the file ' . $filename);
+//      }
+//      fputcsv($f, $tableau);
+//      fclose($f);
+// }
+
+// fputs($f, (chr(0xEF) . chr(0xBB) . chr(0xBF))); // support unicode
 //écrire ligne par ligne
-foreach ($array as $row) {
-	fputcsv($f, $row);
-};
+//foreach ($array as $row) {
+//};
+// lire ligne par ligne
+/*
+while (($row = fgetcsv($f)) !== false) {
+	$data[] = $row;
+}
 // fermer le fichier
 fclose($f);
+*/
