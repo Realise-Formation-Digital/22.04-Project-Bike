b283fc310ced280e4ba56496d94abd39614c1884
Vinko Roditi <vinko.d.roditi@gmail.com>
2022-03-16 16:16:38 +0100

    tests

--- www/index.php
+++ www/index.php
@@ -48,7 +48,7 @@ pre_r($_POST, $inscriptionFullFilled);
                            
                                    <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-   Contact
+   Sign in
  </button>
  
  <!-- Modal -->
@@ -56,7 +56,7 @@ pre_r($_POST, $inscriptionFullFilled);
    <div class="modal-dialog">
      <div class="modal-content">
        <div class="modal-header">
-         <h5 class="modal-title" id="exampleModalLabel">Formulaire d'enregistrement</h5>
+         <h5 class="modal-title" id="exampleModalLabel">Formulaire d'authentification</h5>
          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
        </div>
        <div class="modal-body">
@@ -65,12 +65,12 @@ pre_r($_POST, $inscriptionFullFilled);
           <!-- action et methode ajoutée -->
          <form action="index.php" method="post">
             <div class="mb-3">
-              <label for="Login" class="form-label">Email address</label>
+              <label for="Login" class="form-label">Login:</label>
               <input type="email" class="form-control" id="Login" name="Login" aria-describedby="emailHelp">
              
             </div>
             <div class="mb-3">
-              <label for="Password" class="form-label">Password</label>
+              <label for="Password" class="form-label">Password:</label>
               <input type="password" class="form-control" id="Password" name="Password">
             </div>
            
--- www/index2.php
+++ www/index2.php
@@ -9,7 +9,7 @@ function pre_r($array, $inscriptionOK){
         print_r("Votre inscription a été confirmée");
         
          echo'</pre>';
-     }else{
+     }else if($inscriptionOK == false){
         echo'<pre>';
         print_r("Veuillez vous inscrire je vous prie");
         
@@ -20,7 +20,8 @@ function pre_r($array, $inscriptionOK){
 
 
 
-// var_dump($_POST);
+var_dump($_POST["Login"]);
+
 
 
 if (isset($_POST["exampleInputEmail1"])) {
@@ -44,12 +45,40 @@ if (isset($_POST["exampleInputEmail1"])) {
 }
 // les données du formulaire d'authentification corrrespondent-elle 
 // aux données du formulaire d'enregistrement ?
+
 // if($_POST["exampleInputEmail1"] == $_POST["Login"] &&
 // $_POST["exampleInputPassword1"] == $_POST["Password"] ){
 //     echo("tu es connecté");
 // }else{
 //     echo("tu n'es pas connecté");
 // }
+// if($f = fopen("user.csv")){
+//     fgetcsv($f, 1000, ",");
+
+// }
+$loggedIn = false;
+//prendre le fichier csv
+$filename = "user.csv";
+$f = fopen($filename, 'r');
+//écrire dans le fichier csv
+
+
+
+if($f){
+    
+    while($line = fgetcsv($f, 1000, ",")){
+       
+        if($line == $_POST["Login"].",".$_POST["Password"]){
+            die ("test2");
+            $loggedIn = true;
+            print_r("tu es connecté");
+        }else{
+            
+            return print_r("tu n'es pas connecté");
+            
+        }
+    }
+}
 
 $name = $_POST['name'];
 $emailAddress = $_POST['emailAddress'];
@@ -59,8 +88,6 @@ $message = $_POST['message'];
 // Capturer les données transmises
 
 $tableau = [$_POST['name'], $_POST['emailAddress'], $_POST['message']];
-
-
 
  $fp = fopen('file.csv', 'w');
  
